e23375d2e09c5fb90ed8333b7e2f3cfa555c7af8
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-06-23 18:47:42 -0300

    Update NavMenu.tsx

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 import { Link as ScrollLink } from "react-scroll"
 
 // Visual
@@ -8,21 +8,42 @@ import { NavElement } from "../styles/components/NavMenu"
 import { FiSearch } from "react-icons/fi"
 
 function NavMenu(): JSX.Element {
+	const { pathname } = useLocation()
+
+	function scrollToTop(): void {
+		window.scrollTo(0, 0)
+	}
+
+	useEffect(() => {
+		scrollToTop()
+	}, [])
+
 	return (
 		<NavElement>
 			<div className="tabs">
-				<Link className="headerLink" to="/">
+				<Link
+					onClick={scrollToTop}
+					className={`headerLink ${pathname == "/" && "on"}`}
+					to="/"
+				>
 					INÍCIO
 				</Link>
-				<ScrollLink
-					className="headerLink"
-					activeClass="on"
-					to="catalogue"
-					smooth={true}
-					spy={true}
-				>
-					CATÁLOGO
-				</ScrollLink>
+
+				{pathname == "/" ? (
+					<ScrollLink
+						className="headerLink"
+						activeClass="on"
+						to="catalogue"
+						smooth={true}
+						spy={true}
+					>
+						CATÁLOGO
+					</ScrollLink>
+				) : (
+					<Link className="headerLink on" to={pathname}>
+						CATÁLOGO
+					</Link>
+				)}
 			</div>
 			<div className="searchIcon">
 				<FiSearch />
